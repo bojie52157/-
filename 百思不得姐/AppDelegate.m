0beaf49e368a18e7801f41ac29f4707dd8d34db8
孙 <674504183@qq.com>
2019-08-24 16:19:00 +0800

    创建PCH文件

--- 百思不得姐/AppDelegate.m
+++ 百思不得姐/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "XMGTableBarController.h"
 @interface AppDelegate ()
 
 @end
@@ -17,53 +17,12 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
 
-    //文字属性
-        //普通状态下文字
-    NSMutableDictionary *normalAttri = [NSMutableDictionary dictionary];
-    normalAttri[NSFontAttributeName] = [UIFont systemFontOfSize:14];
-    normalAttri[NSForegroundColorAttributeName] = [UIColor grayColor];
-        //选中状态下的文字
-    NSMutableDictionary *selectedAttri = [NSMutableDictionary dictionary];
-    selectedAttri[NSForegroundColorAttributeName] = [UIColor darkGrayColor];
-    
     //创建窗口
-    self.window = [[UIWindow alloc]initWithFrame:[UIScreen mainScreen].bounds];
-    //创建根控制器
-    UITabBarController *tabBarVC = [[UITabBarController alloc]init];
-    //创建子控制器
-    UITableViewController *vc0 = [[UITableViewController alloc]init];
-    vc0.title = @"精华";
-    [vc0.tabBarItem setTitleTextAttributes:normalAttri forState:UIControlStateNormal];
-    [vc0.tabBarItem setTitleTextAttributes:selectedAttri forState:UIControlStateSelected];
-    vc0.tabBarItem.image = [UIImage imageNamed:@"tabBar_essence_icon"];
-    vc0.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_essence_click_icon"];
-    [tabBarVC addChildViewController:vc0];
+    self.window = [[UIWindow alloc]init];
+    self.window.frame = [UIScreen mainScreen].bounds;
     
-    UIViewController *vc1 = [[UIViewController alloc]init];
-    vc1.title = @"新帖";
-    [vc1.tabBarItem setTitleTextAttributes:normalAttri forState:UIControlStateNormal];
-    [vc1.tabBarItem setTitleTextAttributes:selectedAttri forState:UIControlStateSelected];
-    vc1.tabBarItem.image = [UIImage imageNamed:@"tabBar_new_icon"];
-    vc1.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_new_click_icon"];
-    [tabBarVC addChildViewController:vc1];
-    
-    UITableViewController *vc3 = [[UITableViewController alloc]init];
-    vc3.title = @"关注";
-    [vc1.tabBarItem setTitleTextAttributes:normalAttri forState:UIControlStateNormal];
-    [vc1.tabBarItem setTitleTextAttributes:selectedAttri forState:UIControlStateSelected];
-    vc3.tabBarItem.image = [UIImage imageNamed:@"tabBar_friendTrends_icon"];
-    vc3.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_friendTrends_click_icon"];
-    [tabBarVC addChildViewController:vc3];
-    
-    UIViewController *vc4 = [[UIViewController alloc]init];
-    vc4.title = @"我";
-    [vc1.tabBarItem setTitleTextAttributes:normalAttri forState:UIControlStateNormal];
-    [vc1.tabBarItem setTitleTextAttributes:selectedAttri forState:UIControlStateSelected];
-    vc4.tabBarItem.image = [UIImage imageNamed:@"tabBar_me_icon"];
-    vc4.tabBarItem.selectedImage = [UIImage imageNamed:@"tabBar_me_click_icon"];
-    [tabBarVC addChildViewController:vc4];
     //设置根控制器
-    self.window.rootViewController = tabBarVC;
+    self.window.rootViewController = [[XMGTableBarController alloc]init];
     //设置主窗口
     [self.window makeKeyAndVisible];
     
---
+++ 百思不得姐/XMGTableBarController.m
@@ -0,0 +1,59 @@
+//
+//  XMGTableBarController.m
+//  百思不得姐
+//
+//  Created by 孙 on 2019/8/24.
+//  Copyright © 2019 小情调. All rights reserved.
+//
+
+#import "XMGTableBarController.h"
+
+
+
+@interface XMGTableBarController ()
+
+@end
+
+@implementation XMGTableBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    //文字属性
+    UITabBarItem *item = [UITabBarItem appearance];
+    //普通状态下文字
+    NSMutableDictionary *normalAttri = [NSMutableDictionary dictionary];
+    normalAttri[NSFontAttributeName] = [UIFont systemFontOfSize:14];
+    normalAttri[NSForegroundColorAttributeName] = [UIColor grayColor];
+    [item setTitleTextAttributes:normalAttri forState:UIControlStateNormal];
+    //选中状态下的文字
+    NSMutableDictionary *selectedAttri = [NSMutableDictionary dictionary];
+    selectedAttri[NSForegroundColorAttributeName] = [UIColor darkGrayColor];
+    [item setTitleTextAttributes:selectedAttri forState:UIControlStateSelected];
+    
+    //创建子控制器
+    [self setupOneChildViewController:[[UITableViewController alloc]init] title:@"精华" image:@"tabBar_essence_icon" selectedImage:@"tabBar_essence_click_icon"];
+    [self setupOneChildViewController:[[UIViewController alloc]init] title:@"新帖" image:@"tabBar_new_icon" selectedImage:@"tabBar_new_click_icon"];
+    [self setupOneChildViewController:[[UITableViewController alloc]init] title:@"关注" image:@"tabBar_friendTrends_icon" selectedImage:@"tabBar_friendTrends_click_icon"];
+    [self setupOneChildViewController:[[UIViewController alloc]init] title:@"我" image:@"tabBar_me_icon" selectedImage:@"tabBar_me_click_icon"];
+}
+
+/**
+ 初始化一个子控制器
+
+ @param vc 子控制器
+ @param title 标题
+ @param image 图标
+ @param selectedImage 选中的图标
+ */
+-(void)setupOneChildViewController:(UIViewController *)vc title:(NSString *)title image:(NSString *)image selectedImage:(NSString *)selectedImage{
+    vc.view.backgroundColor = XMGRandomColor;
+    vc.tabBarItem.title = title;
+    vc.tabBarItem.image = [UIImage imageNamed:image];
+    vc.tabBarItem.selectedImage = [UIImage imageNamed:selectedImage];
+    [self addChildViewController:vc];
+    
+}
+
+
+@end
