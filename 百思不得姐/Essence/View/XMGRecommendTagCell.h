f6fd0a77b8b0b70da11c09a4ac13c33faa74d954
孙 <674504183@qq.com>
2019-09-24 18:05:09 +0800

    no message

---
+++ 百思不得姐/Essence/View/XMGRecommendTagCell.h
@@ -0,0 +1,12 @@
+//
+//  XMGRecommendTagCell.h
+//  4期-百思不得姐
+//
+//  Created by xiaomage on 15/10/8.
+//  Copyright © 2015年 xiaomage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XMGRecommendTagCell : UITableViewCell
+@end
---
+++ 百思不得姐/Essence/View/XMGRecommendTagCell.m
@@ -0,0 +1,21 @@
+//
+//  XMGRecommendTagCell.m
+//  4期-百思不得姐
+//
+//  Created by xiaomage on 15/10/8.
+//  Copyright © 2015年 xiaomage. All rights reserved.
+//
+
+#import "XMGRecommendTagCell.h"
+
+
+@interface XMGRecommendTagCell()
+@property (weak, nonatomic) IBOutlet UIImageView *imageListView;
+@property (weak, nonatomic) IBOutlet UILabel *themeNameLabel;
+@property (weak, nonatomic) IBOutlet UILabel *subNumberLabel;
+@end
+
+@implementation XMGRecommendTagCell
+
+
+@end
